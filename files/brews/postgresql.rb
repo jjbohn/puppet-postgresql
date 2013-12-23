a92ab05a5c05f495f7cd54d05a05b110df1de1ff
John Bohn <jjbohn@gmail.com>
2013-12-23 15:51:38 -0500

    Remove patches

--- files/brews/postgresql.rb
+++ files/brews/postgresql.rb
@@ -18,12 +18,6 @@ class Postgresql < Formula
     ]
   end
 
-  # Fix PL/Python build: https://github.com/mxcl/homebrew/issues/11162
-  # Fix uuid-ossp build issues: http://archives.postgresql.org/pgsql-general/2012-07/msg00654.php
-  def patches
-    DATA
-  end
-
   skip_clean :all
 
   fails_with :clang do
@@ -100,27 +94,3 @@ class Postgresql < Formula
     end
   end
 end
-
-__END__
---- a/src/pl/plpython/Makefile  2011-09-23 08:03:52.000000000 +1000
-+++ b/src/pl/plpython/Makefile  2011-10-26 21:43:40.000000000 +1100
-@@ -24,8 +24,6 @@
- # Darwin (OS X) has its own ideas about how to do this.
- ifeq ($(PORTNAME), darwin)
- shared_libpython = yes
--override python_libspec = -framework Python
--override python_additional_libs =
- endif
- 
- # If we don't have a shared library and the platform doesn't allow it
---- a/contrib/uuid-ossp/uuid-ossp.c 2012-07-30 18:34:53.000000000 -0700
-+++ b/contrib/uuid-ossp/uuid-ossp.c 2012-07-30 18:35:03.000000000 -0700
-@@ -9,6 +9,8 @@
-  *-------------------------------------------------------------------------
-  */
-
-+#define _XOPEN_SOURCE
-+
- #include "postgres.h"
- #include "fmgr.h"
- #include "utils/builtins.h"
